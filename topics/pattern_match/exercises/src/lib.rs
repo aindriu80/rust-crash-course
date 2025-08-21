574a6de616fbf33fdad5434e63e02bb6fd09dfbe
Aindriú Mac Giolla Eoin <aindriu80@gmail.com>
2025-08-21 17:44:51 +0100

    Pattern matches - passing the tests

---
+++ topics/pattern_match/exercises/src/lib.rs
@@ -0,0 +1,13 @@
+pub fn num_to_string(num: u32) -> String {
+    match num {
+        0 => "zero".to_string(),
+        1 => "one".to_string(),
+        2 => "two".to_string(),
+        3 => "three".to_string(),
+        _ => "other".to_string(),
+    }
+}
+
+pub fn unwrap_or_default(x: Option<u32>, v: u32) -> u32 {
+    x.unwrap_or(v)
+}
