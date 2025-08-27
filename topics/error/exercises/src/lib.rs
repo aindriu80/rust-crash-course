5d33c4ec8676727c41ee3831366f634166ed89ba
Aindriú Mac Giolla Eoin <aindriu80@gmail.com>
2025-08-27 18:14:55 +0100

    Error handling - passing the tests

---
+++ topics/error/exercises/src/lib.rs
@@ -0,0 +1,15 @@
+#[derive(Debug, PartialEq)]
+pub enum MathError {
+    DivByZero,
+}
+
+pub fn div(x: u32, y: u32) -> Result<u32, MathError> {
+    if y == 0 {
+        return Err(MathError::DivByZero);
+    }
+    Ok(x / y)
+}
+
+pub fn get(v: &[u32], i: usize, default_val: u32) -> u32 {
+    if i < v.len() { v[i] } else { default_val }
+}
